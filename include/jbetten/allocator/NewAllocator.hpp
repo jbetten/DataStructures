39d22d18923c86b45af359c04241047ec5bc10b5
Jonathan Bettencourt <jbetten@gmail.com>
2015-12-05 15:46:38 -0500

    A simple allocator.

---
+++ include/jbetten/allocator/NewAllocator.hpp
@@ -0,0 +1,63 @@
+//
+// Created by jbetten on 12/5/15.
+//
+
+#ifndef DATASTRUCTURES_NEWALLOCATOR_HPP
+#define DATASTRUCTURES_NEWALLOCATOR_HPP
+
+#include <cstdint>
+
+template <std::size_t BlockSize>
+class NewAllocator {
+
+private:
+
+    struct Block {
+        Block *next;
+        std::uint8_t pad[BlockSize - sizeof(Block *)];
+    };
+
+    Block *free_list;
+
+public:
+
+    using Pointer = void *;
+
+    NewAllocator()
+            : free_list(nullptr) {}
+
+    template <typename NodeType>
+    Pointer get(NodeType * const node) {
+        return reinterpret_cast<Pointer>(node);
+    }
+
+    template <typename NodeType>
+    NodeType *get(Pointer ptr) {
+        return reinterpret_cast<NodeType>(ptr);
+    }
+
+    template <typename NodeType>
+    std::pair<Pointer, NodeType *> allocate() {
+        static_assert(sizeof(NodeType) <= BlockSize,
+                      "BlockSize is too small for requested type.");
+
+        Block *block;
+        if (nullptr == free_list) {
+            block = new Block();
+        } else {
+            block = free_list;
+            free_list = free_list->next;
+        }
+        return std::pair<Pointer, NodeType *>(block, new (block) NodeType());
+    }
+
+    template <typename NodeType>
+    void free(NodeType * const node) {
+        node->~NodeType();
+        Block * const block = reinterpret_cast<Block *>(node);
+        block->next = free_list;
+        free_list = block;
+    }
+};
+
+#endif //DATASTRUCTURES_NEWALLOCATOR_HPP
