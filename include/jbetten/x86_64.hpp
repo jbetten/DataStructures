8ce73e379e0e94052a7681ffa981936b34bedb3d
Jonathan Bettencourt <jbetten@gmail.com>
2015-12-05 15:32:12 -0500

    Fix namespace

--- include/jbetten/x86_64.hpp
+++ include/jbetten/x86_64.hpp
@@ -5,7 +5,11 @@
 #ifndef PROJECT_X86_64_HPP
 #define PROJECT_X86_64_HPP
 
-static constexpr std::size_t CacheLineSize = 64;
-static constexpr std::size_t PageSize = 4096;
+namespace jbetten {
+
+    static constexpr std::size_t CacheLineSize = 64;
+    static constexpr std::size_t PageSize = 4096;
+
+};
 
 #endif //PROJECT_X86_64_HPP
